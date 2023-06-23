3ec2606e6ac31ad3834b50b3bc76627675e0410f
Chavannes-de-Bogis <tarek.schuetz@stud.kbw.ch>
2023-06-23 14:26:38 +0200

    added pages with routing

--- gallery/src/App.js
+++ gallery/src/App.js
@@ -1,26 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Gallery from './Pages/Gallery';
+import NotFound from './Pages/NotFound';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path='gallery' element={<Gallery />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
-}
-
-export default App;
+  )
+};

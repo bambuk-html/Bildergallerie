247d72048a19664090215bc6ef9b9477c89aa533
Chavannes-de-Bogis <tarek.schuetz@stud.kbw.ch>
2023-06-27 09:38:05 +0200

    Bilder werden angezeigt auf "gallery" seite

--- gallery/src/Components/imageGallery.js
+++ gallery/src/Components/imageGallery.js
@@ -1,37 +1,45 @@
 import React from 'react';
+import image1 from "./images/image_1.jpg";
+import image2 from "./images/image_2.jpg";
+import image3 from "./images/image_3.jpg";
+import image4 from "./images/image_4.jpg";
+import image5 from "./images/image_5.jpg";
+import image6 from "./images/image_6.jpg";
+import image7 from "./images/image_7.jpg";
+import image8 from "./images/image_8.jpg";
+import image9 from "./images/image_9.jpg";
+import image10 from "./images/image_10.jpg";
+import image11 from "./images/image_11.jpg";
+import image12 from "./images/image_12.jpg";
+import image13 from "./images/image_13.jpg";
+import image14 from "./images/image_14.jpg";
+import image15 from "./images/image_15.jpg";
 
 function ImageGallery() {
-  const imageFolder = './images';
-
   const imageFilenames = [
-    '1.jpg',
-    '2.jpg',
-    '3.jpg',
-    '4.jpg',
-    '5.jpg',
-    '6.jpg',
-    '7.jpg',
-    '8.jpg',
-    '9.jpg',
-    '10.jpg',
-    '11.jpg',
-    '12.jpg',
-    '13.jpg',
-    '14.jpg',
-    '15.jpg'
+    image1,
+    image2,
+    image3,
+    image4,
+    image5,
+    image6,
+    image7,
+    image8,
+    image9,
+    image10,
+    image11,
+    image12,
+    image13,
+    image14,
+    image15
   ];
 
   return (
     <div>
       <h2>Image Gallery</h2>
       <div className="image-container">
-        {imageFilenames.map((filename) => (
-          <img
-            key={filename}
-            src={`${imageFolder}/${filename}`}
-            alt={filename}
-            className="gallery-image"
-          />
+        {imageFilenames.map((image, index) => (
+          <img key={index} src={image} alt={`Image ${index + 1}`} />
         ))}
       </div>
     </div>
